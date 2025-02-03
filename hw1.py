eebf11c828c97765303028caa76cd824d60a291e
Chienyu Sun <chienyu_sun@berkeley.com>
2025-02-02 20:01:49 -0800

    Adding hw1.py

---
+++ hw1.py
@@ -0,0 +1,13 @@
+# pwd: print working directory; use it to show the current directory; -P to show the path from root directory to the current directory 
+# cd: change directory; use it to switch to a different directory; -P to switch to the root directory 
+# ls: list files; use it to lists the files in the current directory; -a to show all files, even hidden ones too
+# mkdir: make new directory; use it to create a new directory; -p to create a directory and a nested directory at the same time
+# cp: copy a file; use it to copy files into different directories; -r to copy a directory
+# mv: move a file; use it to move files into different directories; -n to move a file without overwriting an existing file
+
+# 1. Git allows code to be stored in local repositories on the computer. Github allows code to be stored in repositories on the internet. Git Bash allows Windows users to use Git command lines.
+# 2. The terminal is the entire app used to write commands in, while the command line is each individual line that the commands are written on.
+# 3. Windows Powershell is a command-line shell on a Windows system. Git Bash is an app that allows the scripting language Bash to be used on Windows systems.
+# 4. Anaconda holds lots of different open-source packages for programming languages, and is easy to use because of its many pre-installed packages. Conda is a package management system, which is a set of tools that helps manage the software of the computer. Python is a programming language.
+# 5. Jupyter Notebook is a web-based platform used for editing and explaining code. Jupyter Lab is the environment used to create the notebooks.
+# 6. VS Code is a code editor used to write and edit code. 
