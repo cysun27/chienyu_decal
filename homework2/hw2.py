8e3f708a90d529e5fd9ba29b4810b676a9819815
Chienyu Sun <chienyu_sun@berkeley.com>
2025-02-06 15:43:40 -0800

    Adding hw2.py

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,29 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+# A: Call cd ../../ to go navigate back two directories into ~/Desktop
+
 # 2) What does ~/ mean?
+# A: ~/ means the beginning of a path, starting from the home directory
+
 # 3) What's the difference between and abolute and a relative path in your terminal?
+# A: A relative path is a path that is relative to the current directory, while an absolute path begins from the home directory
+
 # 4) What command would bring you back to your home directory?
+# A: Call cd with nothing after it to return to the home directory
+
 # 5) If you called rm ./ in your current working directory, what would it do? 
+# It would permanently remove the current working directory
+
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+# A: "git add" adds files or directories from the working directory into the staging area, "git commit" saves everything in the staging area onto the local repository, "git push" puts everything in the local repository onto the Internet
+
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+# A: It will tell you the status of the local repository, and if it is called after "git add", then it would show that there are files in the staging area that have not been committed yet.
+
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# A: I couldn't figure out why lots of errors would pop up after calling "git add", but turns out I was in the wrong directory so it didn't have permission to access the files I was adding. I jumped to the correct directory and tried again and it worked
+
 # 9) How does cloning a repository differ from pulling from a repository?
+# A: Cloning means that you are making a copy of the repository, while pulling means you are getting any new changes that have been made to the repository
+
 # 10) Tell me a fun fact!
+# A: I have a little experience working with Python to do calculations, but I have never used the terminal before this class!
